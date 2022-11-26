0b5f5ae62e5181da90c85eb18af0c730ff3e333a
kY49 <kay2049@icloud.com>
2022-11-26 18:27:59 +0200

    Adding skeleton of RedisConnector responsible for connecting to redis

---
+++ src/main/redis_connector.py
@@ -0,0 +1,15 @@
+import redis 
+
+class RedisConnector(object):
+
+    def __init__(self)->None:pass
+
+    def __connect__(self):pass
+
+    def __disconneßct__(self)->None:pass 
+
+    def __create_connection__(self)->None:pass
+
+
+
+
