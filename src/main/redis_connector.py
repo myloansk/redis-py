4a282c58b6db1b1ad74b541f447eae0c88042400
kY49 <kay2049@icloud.com>
2022-11-26 18:33:43 +0200

    Adding redis client's class props

--- src/main/redis_connector.py
+++ src/main/redis_connector.py
@@ -2,11 +2,18 @@ import redis
 
 class RedisConnector(object):
 
-    def __init__(self)->None:pass
+    def __init__(self, hostName:str, 
+                dbName:str, userName:str, password:str = None,
+                port:int = 6379 )->None:
+        self.hostName = hostName
+        self.port = port 
+        self.dbName = dbName
+        self.userName = userName
+        self.password = password
 
     def __connect__(self):pass
 
-    def __disconneßct__(self)->None:pass 
+    def __disconnect__(self)->None:pass 
 
     def __create_connection__(self)->None:pass
 
